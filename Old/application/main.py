ab4ff9f6e0299096084768981392228f4c11dc31
Mete <Metterre@outlook.com.tr>
2022-09-01 22:56:47 +0300

    updated

--- Old/application/main.py
+++ Old/application/main.py
@@ -2,15 +2,11 @@ from tkinter import Tk
 from database.database import database
 from userInterface.gui3 import mainRoot
 data = database()
-rootConfig = mainRoot("MyMathcer",data)
-
-
-
-
+rootConfig = mainRoot("MyMathcer", data)
 
 
 if __name__ == "__main__":
-    # This is main tkinter root for application 
+    # This is main tkinter root for application
     root = Tk()
     data.guiRoot = root
     rootConfig.mainRootConfig(data.guiRoot)
